3c2b235d02e9edb347430a0223046f8875ef4ac2
vineetrastogi <vineetrastogi@gmail.com>
2015-02-06 23:41:40 -0800

    working on everything

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,16 +2,44 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  user = User.find_by(username: params[:username])
+
+  if user.password == params[:password]
+    session[:user_id] = user.id
+    redirect '/login'
+  end
+  "Please login, BRUH."
+end
+
+get '/teams' do
+  # @category = params[:splat][0].to_s
+  # @myteam = Team.first
+  # @myteam.players << Player.find(params[:id])
+  erb :teams
+end
+
+post '/teams/:id' do
+  # p params
+  @id = Player.find(params[:player_id])
+end
+
 get '/category/*' do
   @category = params[:splat][0].to_s
   @players = helper_category(@category)
   erb :players
 end
 
-get '/teams/player/add/:id' do
-  @interceptions = Player.where.not(interception: nil)
-  @myteam = Team.first
-  @myteam.players << Player.find(params[:id])
-  erb :index
+post '/category/*' do
+  added_player = params[:player_id]
+end
+
+post '/login' do
+  erb :login
+end
+
+get '/logout' do
+  session.delete :user_id
+  redirect '/'
 end
 
