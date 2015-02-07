860b9be41f923bcb2641be507cf2ba57fdbc6f59
vineetrastogi <vineetrastogi@gmail.com>
2015-02-07 15:25:33 -0800

    created migration for bcrypt, and am implementing login right now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,29 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many :teams
-  # Remember to create a migration!
-end
+  include BCrypt
 
+  def password
+    @password ||= Password.new(password_hash)
+  end
 
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
+  end
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+    give_token
+    else
+      redirect_to home_url
+    end
+  end
---
+++ db/migrate/20150207151406_updating_password_to_password_hash.rb
@@ -0,0 +1,5 @@
+class UpdatingPasswordToPasswordHash < ActiveRecord::Migration
+  def change
+    rename_column(:users, :password, :password_hash )
+  end
+end
