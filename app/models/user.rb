e5b0147f4ae5603a21c80b5fe6ceee3b183d4185
vineetrastogi <vineetrastogi@gmail.com>
2015-02-08 09:01:16 -0800

    finished basic styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,8 @@ class User < ActiveRecord::Base
     @user = User.find_by_email(params[:email])
     if @user.password == params[:password]
       give_token
-    # else
-    #   redirect_to home_url
+      # else
+      #   redirect_to home_url
     end
   end
 end
